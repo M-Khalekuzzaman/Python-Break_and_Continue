c19f1f08a21be9c2842dfb49843282ffae60458b
Kaochar <94604055+M-Khalekuzzaman@users.noreply.github.com>
2021-11-22 21:21:40 +0600

    Add files via upload

---
+++ Break and continue.py
@@ -0,0 +1,10 @@
+#using keyword break and continue
+number = int(input(("Enter your integer number : ")))
+
+i = 1
+while i<=number :
+    if i == 20 :
+        continue
+    print(i)
+    i = i + 1
+print("program end")
